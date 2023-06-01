79a2a2797d467284b8ea6ef4e91f0925cbd43743
Shirov Ivan <117617823+Carnifex111@users.noreply.github.com>
2023-06-01 22:37:41 +0500

    2

---
+++ 02.Python/branches.py
@@ -0,0 +1,9 @@
+box_one = "Васька"
+box_two = "Петька"
+
+if box_one == "Васька":
+    print("True! В переменной box_one значение Васька")
+elif box_two == "Петька":
+    print("True! В переменной box_two значение Васька")
+else:
+    print("В переменной box_one не Васька")
---
+++ 02.Python/loop.py
@@ -0,0 +1,20 @@
+my_list = [1, 2, 3, 4, 5]
+for element in my_list:
+    # выполняемый блок кода для каждого элемента списка
+    print(element)
+
+my_string = "Hello"
+for char in my_string:
+    # выполняемый блок кода для каждого символа в строке
+    print(char)
+
+
+for i in range(5):
+    # выполняемый блок кода 5 раз (от 0 до 4)
+    print(i)
+
+i = 0
+while i < 5:
+    # выполняемый блок кода до тех пор, пока i меньше 5
+    print(i)
+    i += 1
